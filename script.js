8cc64d85eb3d2ee4b98b29c0d94701da7945c4ad
Ryan Mc Goldrick <ryanmcgold2017@gmail.com>
2021-12-04 18:01:05 +0000

    update

--- script.js
+++ script.js
@@ -14,10 +14,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(500);
 
 
-
 // lighting and helpers
-const pointLight = new THREE.PointLight(0xfefddc, 2),
-    ambientLight = new THREE.AmbientLight(0xffffff, 0.8),
+const pointLight = new THREE.PointLight(0xfefddc, 1),
+    ambientLight = new THREE.AmbientLight(0xffffff, 0.1),
     lightHelper = new THREE.PointLightHelper(pointLight),
     gridHelper = new THREE.GridHelper(800, 50)
 
@@ -25,7 +24,6 @@ pointLight.position.set(0, 0, 0)
 scene.add(pointLight, ambientLight, /*lightHelper, gridHelper*/ )
 
 // controls
-const controls = new OrbitControls(camera, renderer.domElement);
 
 
 
@@ -45,24 +43,16 @@ function addTorus(radius, tubeRadius, color) {
     return torus;
 
 }
-let rings = [
-    addTorus(200, 0.1, "grey"),
-    addTorus(100, 1, "grey"),
-    addTorus(70, 1, "grey"),
-    addTorus(600, 0.1, "grey"),
-]
-rings[0].rotateX(83 * Math.PI / 180);
-rings[3].rotateX(97 * Math.PI / 180);
 // random stars
 function addStar() {
     const geometry = new THREE.SphereGeometry(0.15, 24, 24);
     const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
     const star = new THREE.Mesh(geometry, material);
-    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(1000));
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(10000));
     star.position.set(x, y, z);
     scene.add(star)
 }
-Array(3000).fill().forEach(addStar)
+Array(1000).fill().forEach(addStar)
 
 // moon
 const skyTexture = new THREE.TextureLoader().load("images/background.jpg");
@@ -75,7 +65,7 @@ scene.add(sky);
 const sunTexture = new THREE.TextureLoader().load("images/sun.jpg");
 const sun = new THREE.Mesh(
     new THREE.SphereGeometry(50, 32, 32),
-    new THREE.MeshStandardMaterial({ map: sunTexture, emissive: 0xf5902c, emissiveIntensity: 0.5 })
+    new THREE.MeshBasicMaterial({ map: sunTexture, emissive: 0xf5902c, emissiveIntensity: 0.5 })
 );
 scene.add(sun);
 // https://stemkoski.github.io/Three.js/
@@ -90,13 +80,6 @@ var sprite = new THREE.Sprite(spriteMaterial);
 sprite.scale.set(400, 400, 1.0);
 sun.add(sprite); // this centers the glow at the mesh
 
-const boxTexture = new THREE.TextureLoader().load("images/francheesestick.png");
-const box = new THREE.Mesh(
-    new THREE.BoxGeometry(60, 60, 60),
-    new THREE.MeshPhysicalMaterial({ map: boxTexture, emissive: "white", emissiveIntensity: 10 })
-);
-//scene.add(box);
-
 function addSphere(textureURL, radius, x, y, z) {
     const sphereTexture = new THREE.TextureLoader().load(textureURL);
     const sphere = new THREE.Mesh(
@@ -108,16 +91,25 @@ function addSphere(textureURL, radius, x, y, z) {
     return sphere;
 }
 let planets = [
-        addSphere("images/mercury.jpg", 10, 100, 20, 0),
-        addSphere("images/venus.jpg", 15, -150, -5, 0),
-        addSphere("images/earth.jpg", 16, 200, 10, 0),
-        addSphere("images/mars.jpg", 12, -250, 5, 0),
-        addSphere("images/jupiter.jpg", 50, 300, -20, 0),
-        addSphere("images/saturn.jpg", 40, -350, -10, 0),
-        addSphere("images/uranus.jpg", 30, 400, -20, 0),
-        addSphere("images/neptune.jpg", 35, -450, 30, 0),
-    ]
-    // stl loader
+    addSphere("images/mercury.jpg", 2, 100, 0, 0),
+    addSphere("images/venus.jpg", 6, -350, -5, 0),
+    addSphere("images/earth.jpg", 7, 500, 10, 0),
+    addSphere("images/mars.jpg", 5, -850, 5, 0),
+    addSphere("images/jupiter.jpg", 50, 1000, -20, 0),
+    addSphere("images/saturn.jpg", 45, -1350, -10, 0),
+    addSphere("images/uranus.jpg", 30, 1600, -20, 0),
+    addSphere("images/neptune.jpg", 35, -1950, 30, 0),
+]
+let planetCores = [
+        addSphere("", 0, 100, 0, 0),
+        addSphere("", 0, -350, -5, 0),
+        addSphere("", 0, 500, 10, 0),
+        addSphere("", 0, -850, 5, 0),
+        addSphere("", 0, 1000, -20, 0),
+        addSphere("", 0, -1350, -10, 0),
+        addSphere("", 0, 1600, -20, 0),
+        addSphere("", 0, -1950, 30, 0),
+    ] // stl loader
 let model;
 
 function loadSTLModel(fileURL, color, metalness, roughness, opacity, transparent, transmission, clearcoat, clearcoatRoughness) {
@@ -146,7 +138,6 @@ function loadSTLModel(fileURL, color, metalness, roughness, opacity, transparent
         }
     )
 }
-loadSTLModel("./models/4dCube.stl", "red", 0.7, 0, 1, false, 0, 0, 0)
 
 function rotateAboutPoint(obj, point, axis, theta, pointIsWorld) {
     pointIsWorld = (pointIsWorld === undefined) ? false : pointIsWorld;
@@ -165,7 +156,44 @@ function rotateAboutPoint(obj, point, axis, theta, pointIsWorld) {
 
     obj.rotateOnAxis(axis, theta); // rotate the OBJECT
 }
-// anim loop
+let planetUpdate = () => {
+        for (let i = 0; i < planets.length - 1; i++) {
+            planets[i].position.x = planetCores.position.x;
+            planets[i].position.y = planetCores.position.y;
+            planets[i].position.z = planetCores.position.z;
+            planets[i].rotation.y += 0.1
+        }
+    }
+    // anim loop
+let zoom = 0;
+let index = 0;
+document.addEventListener("keydown", (e) => {
+    if (e.key == "ArrowDown") {
+        zoom += 1;
+    }
+    if (e.key == "ArrowUp" && zoom >= 10) {
+        zoom -= 1;
+    }
+    if (e.key == "ArrowLeft") {
+        if (index >= 1 && index <= planets.length - 1) {
+            index--;
+        }
+        if (index == 0) {
+            index = planets.length - 1;
+        }
+        if (index > planets.length - 1) {
+            index = 0;
+        }
+    }
+    if (e.key == "ArrowRight") {
+        if (index >= 0 && index <= planets.length - 2) {
+            index--;
+        }
+        if (index > planets.length - 1) {
+            index = 0;
+        }
+    }
+})
 
 function animate() {
     requestAnimationFrame(animate);
@@ -178,19 +206,20 @@ function animate() {
         planets[i].rotation.y += 0.01
     }
     sun.rotation.y += 0.01
-    rotateAboutPoint(planets[0], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.075, true);
-    rotateAboutPoint(planets[1], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.065, true);
-    rotateAboutPoint(planets[2], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.04, true);
-    rotateAboutPoint(planets[3], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.025, true);
-    rotateAboutPoint(planets[4], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.005, true);
-    rotateAboutPoint(planets[5], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.004, true);
-    rotateAboutPoint(planets[6], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.002, true);
-    rotateAboutPoint(planets[7], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.001, true);
-    rings[1].rotation.x += 0.1;
-    rings[2].rotation.y += 0.1;
-
+    rotateAboutPoint(planetCores[0], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.0075, true);
+    rotateAboutPoint(planetCores[1], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.0065, true);
+    rotateAboutPoint(planetCores[2], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.004, true);
+    rotateAboutPoint(planetCores[3], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.0025, true);
+    rotateAboutPoint(planetCores[4], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.0005, true);
+    rotateAboutPoint(planetCores[5], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.0004, true);
+    rotateAboutPoint(planetCores[6], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.0002, true);
+    rotateAboutPoint(planetCores[7], new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 1, 0), 0.0001, true);
+    camera.position.x = planets[index].position.x - planets[index].geometry.parameters.radius * 4 - zoom;
+    camera.position.y = planets[index].position.y;
+    camera.position.z = planets[index].position.z - planets[index].geometry.parameters.radius * 4 - zoom;
+    camera.lookAt(planets[2].position)
+    console.log(camera.position);
     renderer.render(scene, camera);
-    controls.update();
 }
 
 animate()
